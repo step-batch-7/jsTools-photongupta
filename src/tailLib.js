4e0271e5376f506233bfea3a5acf3429fa41ff8a
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-21 17:41:58 +0530

    Added funciton --> selectLast10Lines
    this will take the number of lines and file content and give the last 10
    lines of given text.

--- src/tailLib.js
+++ src/tailLib.js
@@ -2,4 +2,11 @@ const formatContent = function(last10Lines) {
   return last10Lines.join("\n");
 };
 
-module.exports = { formatContent };
+const selectLast10Lines = function(contentAndNoOfLines) {
+  const last10Lines = contentAndNoOfLines.content
+    .split("\n")
+    .slice(-contentAndNoOfLines.noOfLines);
+  return last10Lines;
+};
+
+module.exports = { formatContent, selectLast10Lines };
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,4 @@
-const { formatContent } = require("../src/tailLib");
+const { formatContent, selectLast10Lines } = require("../src/tailLib");
 const assert = require("assert");
 
 describe("formatContent", function() {
@@ -6,5 +6,16 @@ describe("formatContent", function() {
     let actual = formatContent(["aa", "bb", "cc", "dd"]);
     let expected = "aa\nbb\ncc\ndd";
     assert.strictEqual(actual, expected);
+  });
+});
+
+describe("selectLast10Lines", function() {
+  it("should give last 10 lines of the given content", function() {
+    let actual = selectLast10Lines({
+      content: "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm",
+      noOfLines: 10
+    });
+    let expected = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+    assert.deepStrictEqual(actual, expected);
   });
 });
