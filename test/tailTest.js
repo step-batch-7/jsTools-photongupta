d939ea8f44ba6f8198a3a39f2e92200111207250
Rashmi Gupta <rashmi224141@gmail.com>
2019-12-22 14:50:11 +0530

    Added test for selectLast10Lines
    function will give whole content in array if the file content have less
    than 10 lines.

--- test/tailTest.js
+++ test/tailTest.js
@@ -14,12 +14,21 @@ describe("formatContent", function() {
 });
 
 describe("selectLast10Lines", function() {
-  it("should give last 10 lines of the given content", function() {
+  it("should give array of last 10 lines of the given content", function() {
     const actual = selectLast10Lines({
       content: "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm",
       noOfLines: 10
     });
     const expected = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should give array of lines of content if the content contains less than 10 line", function() {
+    const actual = selectLast10Lines({
+      content: "c\nd\ne\nf",
+      noOfLines: 10
+    });
+    const expected = ["c", "d", "e", "f"];
     assert.deepStrictEqual(actual, expected);
   });
 });
